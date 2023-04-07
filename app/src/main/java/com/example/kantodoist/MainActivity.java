c33645257b167faf9db7a91c7e2a945c0a51a0fd
sharkz <mosenjemudogoz@gmail.com>
2023-04-07 16:51:54 +0300

    viewpager

--- app/src/main/java/com/example/kantodoist/MainActivity.java
+++ app/src/main/java/com/example/kantodoist/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.kantodoist;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        viewPager=findViewById(R.id.pager);
+        tabLayout=findViewById(R.id.tabs);
+
+        setSupportActionBar(toolbar);
+
+        SectionPagerAdapter adapter=new SectionPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    private class SectionPagerAdapter extends FragmentPagerAdapter{
+
+        public SectionPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:return new TodoFragment();
+                case 1:return new DoneFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0: return "Todo";
+                case 1: return "Done";
+            }
+            return null;
+        }
     }
 }
